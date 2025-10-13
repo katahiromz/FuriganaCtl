3b2b7588f58de29157b7b4f00fa2360f06b7153a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-10-13 21:44:40 +0900

    handle WM_SETTEXT and WM_GETTEXT

--- base_textbox.cpp
+++ base_textbox.cpp
@@ -23,8 +23,8 @@ struct base_textbox_impl_t {
     HFONT m_font = nullptr;
     bool m_own_font = false;
     WNDPROC m_old_wndproc = nullptr;
-    wchar_t *m_text_cache = nullptr;
-    INT m_text_cache_capacity = 0;
+    wchar_t *m_text = nullptr;
+    INT m_text_capacity = 0;
 
     base_textbox_impl_t(HWND hwnd, base_textbox_t *self) {
         m_self = self;
@@ -34,15 +34,18 @@ struct base_textbox_impl_t {
     ~base_textbox_impl_t() {
         if (m_own_font && m_font)
             ::DeleteObject(m_font);
-        delete[] m_text_cache;
+        delete[] m_text;
     }
+
+    void paint_client(HWND hwnd, HDC hDC = nullptr);
 
     BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);
     void OnDestroy(HWND hwnd);
     HFONT OnGetFont(HWND hwnd);
     void OnSetFont(HWND hwndCtl, HFONT hfont, BOOL fRedraw);
+    INT OnGetText(HWND hwnd, int cchTextMax, LPTSTR lpszText);
+    void OnSetText(HWND hwnd, LPCTSTR lpszText);
     void OnPaint(HWND hwnd);
-    void paint_client(HWND hwnd, HDC hDC = nullptr);
 };
 
 // WM_CREATE
@@ -60,6 +63,8 @@ BOOL base_textbox_impl_t::OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct) {
         m_font = nullptr;
         m_own_font = false;
     }
+
+    OnSetText(hwnd, lpCreateStruct->lpszName);
 
     return TRUE;
 }
@@ -91,6 +96,44 @@ void base_textbox_impl_t::OnSetFont(HWND hwndCtl, HFONT hfont, BOOL fRedraw) {
 
     if (fRedraw)
         ::InvalidateRect(hwndCtl, nullptr, TRUE);
+}
+
+// WM_GETTEXT
+INT base_textbox_impl_t::OnGetText(HWND hwnd, int cchTextMax, LPTSTR lpszText) {
+    lstrcpynW(lpszText, m_text, cchTextMax);
+    return lstrlenW(lpszText) + 1;
+}
+
+// WM_SETTEXT
+void base_textbox_impl_t::OnSetText(HWND hwnd, LPCTSTR lpszText) {
+    if (!lpszText) lpszText = L"";
+
+    INT text_len = lstrlenW(lpszText);
+    INT required_capacity = text_len + 1;
+
+    if (m_text_capacity >= 8 * required_capacity) {
+        delete[] m_text;
+        m_text = new(std::nothrow) wchar_t[required_capacity];
+        if (!m_text) {
+            out_of_memory();
+            m_text_capacity = 0;
+            return;
+        }
+        m_text_capacity = required_capacity;
+    } else if (required_capacity > m_text_capacity) {
+        delete[] m_text;
+        const INT extra = 1024;
+        m_text = new(std::nothrow) wchar_t[required_capacity + extra];
+        if (!m_text) {
+            out_of_memory();
+            m_text_capacity = 0;
+            return;
+        }
+        m_text_capacity = required_capacity + extra;
+    }
+
+    lstrcpynW(m_text, lpszText, required_capacity);
+    ::InvalidateRect(hwnd, nullptr, TRUE);
 }
 
 // WM_PAINT
@@ -222,6 +265,8 @@ base_textbox_t::window_proc_inner(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
         HANDLE_MSG(hwnd, WM_CREATE, m_pimpl->OnCreate);
         HANDLE_MSG(hwnd, WM_DESTROY, m_pimpl->OnDestroy);
         HANDLE_MSG(hwnd, WM_PAINT, m_pimpl->OnPaint);
+        HANDLE_MSG(hwnd, WM_GETTEXT, m_pimpl->OnGetText);
+        HANDLE_MSG(hwnd, WM_SETTEXT, m_pimpl->OnSetText);
         HANDLE_MSG(hwnd, WM_GETFONT, m_pimpl->OnGetFont);
         HANDLE_MSG(hwnd, WM_SETFONT, m_pimpl->OnSetFont);
     case WM_ERASEBKGND:
@@ -240,35 +285,9 @@ void base_textbox_t::draw_client(HWND hwnd, HDC dc, RECT *client_rc) {
 
     ::FillRect(dc, client_rc, ::GetSysColorBrush(COLOR_WINDOW));
 
-    INT text_len = ::GetWindowTextLengthW(hwnd);
-    INT required_capacity = text_len + 1;
-
-    if (m_pimpl->m_text_cache_capacity >= 8 * required_capacity) {
-        delete[] m_pimpl->m_text_cache;
-        m_pimpl->m_text_cache = new(std::nothrow) wchar_t[required_capacity];
-        if (!m_pimpl->m_text_cache) {
-            out_of_memory();
-            m_pimpl->m_text_cache_capacity = 0;
-            return;
-        }
-        m_pimpl->m_text_cache_capacity = required_capacity;
-    } else if (required_capacity > m_pimpl->m_text_cache_capacity) {
-        delete[] m_pimpl->m_text_cache;
-        m_pimpl->m_text_cache = new(std::nothrow) wchar_t[required_capacity];
-        if (!m_pimpl->m_text_cache) {
-            out_of_memory();
-            m_pimpl->m_text_cache_capacity = 0;
-            return;
-        }
-        m_pimpl->m_text_cache_capacity = required_capacity;
-    }
-
-    wchar_t *text = m_pimpl->m_text_cache;
-    ::GetWindowTextW(hwnd, text, required_capacity);
-
     INT old_mode = ::SetBkMode(dc, TRANSPARENT);
     ::SetTextColor(dc, GetSysColor(COLOR_WINDOWTEXT));
-    ::DrawText(dc, text, -1, client_rc, DT_LEFT | DT_TOP | DT_EDITCONTROL | DT_EXPANDTABS);
+    ::DrawText(dc, m_pimpl->m_text, -1, client_rc, DT_LEFT | DT_TOP | DT_EDITCONTROL | DT_EXPANDTABS);
     ::SetBkMode(dc, old_mode);
 }
 
